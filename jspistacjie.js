29643751dde636d591a422de7664557582bc9c6e
pistacjie <118319759+pistacjie@users.noreply.github.com>
2022-11-16 07:35:08 +0700

    Add files via upload

---
+++ jspistacjie.js
@@ -0,0 +1,27 @@
+const txtElement = ['Pistacjie', 'Gelato', 'House'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+    if(count== txtElement.length){
+        count = 0;
+}
+
+currentTxt = txtElement[count];
+
+words = currentTxt.slice(0, ++txtIndex);
+document.querySelector('.efek-ngetik').textContent = words;
+
+if(words.length == currentTxt.length){
+    count++;
+    txtIndex = 0;
+}
+
+setTimeout(ngetik, 500);
+
+
+})();
+
